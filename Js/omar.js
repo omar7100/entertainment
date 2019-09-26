3f545de88149b986dae137575d239af5a482518e
omar <omar.s.alshoul@gmail.com>
2019-09-26 12:51:35 +0300

    ok

--- Js/omar.js
+++ Js/omar.js
@@ -1,22 +1,17 @@
 'use strict'
 
 //first java code
-
 var today= new Date();
 var hourNow = today.getHours();
 var greeting;
-
 var chose = prompt('What do you like most? ');
-
 if (hourNow >= 18) {
-   greeting = 'let us have a show!';
+   greeting = 'deadwood!';
 } else if (hourNow >= 12) {
-   greeting = ' The great series DEAD WOOD!';
+   greeting = ' talkhsow!';
 } else if (hourNow >= 0) {
-   greeting = 'Talk show with Steve Harvey!';
+   greeting = 'Tvtime!';
 } else {
    greeting = 'Whaever You Want!' ;
 }
-document.write(  greeting );
-
-
+document.write( ' <h3>' + greeting + ' </ h3>');
